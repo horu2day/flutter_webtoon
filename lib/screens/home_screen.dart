436f40e8e66982a0cbfb1debf0ee381d4fb70330
horu2day <horu2day@gmail.com>
2023-01-17 00:06:05 +0900

    ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,9 +25,20 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text('There is DAta!');
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+            );
           }
-          return const Text('loading....');
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
