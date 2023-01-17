a68138021ee16c4ad08c619e6ac77305c57934f6
horu2day <horu2day@gmail.com>
2023-01-18 07:42:23 +0900

    detail

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
